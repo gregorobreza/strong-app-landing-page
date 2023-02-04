f217b77ea085da28a44d1fd46ae087b654541725
Gregor Obreza <gobreza@gmail.com>
2023-02-04 10:59:47 +0100

    mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <Container size={450} px="xs" sx={{ display: "flex", height: "100vh", alignItems: "center"}}>
+      <Container size={450} px="xs" sx={{ display: "flex", height: "100vh", alignItems: "center", justifyContent:"center"}}>
       
           <TestCard />
    
