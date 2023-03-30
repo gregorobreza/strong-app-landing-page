ec90848b897050b4b333e9e70c8e7874df94b8d3
Gregor Obreza <gobreza@gmail.com>
2023-03-30 19:47:43 +0200

    second try

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    GOOGLE_RECAPTCHA_SECRET_KEY: process.env.GOOGLE_RECAPTCHA_SECRET_KEY,
+  },
 }
 
 module.exports = nextConfig
